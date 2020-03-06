cb73ef5a23375747b59877043fab6c520b955a6e
Carlo Colombo <ccolombo@pivotal.io>
2020-03-06 08:04:21 +0000

    add uptime function

--- lib/mood_light/server.ex
+++ lib/mood_light/server.ex
@@ -20,11 +20,23 @@ defmodule MoodLight.Server do
       |> send_resp(200, Jason.encode!(%{ola: :foo}))
     end
 
+    defp uptime do
+        {ut, _} = :erlang.statistics(:wall_clock)
+        {d, {h, m, s}} = :calendar.seconds_to_daystime(div(ut,1000))
+        Enum.filter([
+            d>0 && "#{d} days, ",
+            d+h>0 && "#{h} hours, ",
+            d+h+m>0 && "#{m} minutes and ",
+            "#{s} seconds"
+        ], &(&1))
+        |> Enum.join
+    end
+
     get "/info" do
         conn
         |> put_resp_header("content-type", "application/json; charset=utf-8")
         |> send_resp(200, Jason.encode!(%{
-            uptime: Toolshed.Unix.uptime
+            uptime: uptime()
         }))
       end
   
